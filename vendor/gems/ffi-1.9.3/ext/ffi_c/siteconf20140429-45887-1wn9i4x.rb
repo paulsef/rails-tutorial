ffd9a71ba3931f7dbb7a9c2bad27f7717cf15190
paul <minton.paul@gmail.com>
2014-04-29 21:29:11 -0700

    added some dependecies i think

---
+++ vendor/gems/ffi-1.9.3/ext/ffi_c/siteconf20140429-45887-1wn9i4x.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140429-45887-1e12q4k"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/gems/ffi-1.9.3/ext/ffi_c/siteconf20140429-45908-6cnwoe.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140429-45908-1dcdch9"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
